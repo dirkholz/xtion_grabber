98c6c84b165a0e39db8105ce1204edcbe91d7fcf
Dirk Holz <holz@ais.uni-bonn.de>
2016-03-02 15:31:53 +0100

    now updating if the kernel_module directory already exists

--- scripts/install_kernel_module.sh
+++ scripts/install_kernel_module.sh
@@ -5,8 +5,11 @@ xtion_grabber=$(rospack find xtion_grabber)
 module_dir=${xtion_grabber}/kernel_module
 
 if [ ! -d "${module_dir}" ]; then
-	echo "Retrieving xtion repository"
-	mkdir -p ${module_dir} && git clone ${module_git} ${module_dir} && git checkout develop
+    echo "Retrieving xtion repository"
+    mkdir -p ${module_dir} && git clone ${module_git} ${module_dir} && git checkout develop
+else
+    echo "Directory  ${module_dir} already exists. Updating ..."
+    cd ${module_dir} && git pull
 fi
 
 echo "Compiling and installing the xtion kernel module"
